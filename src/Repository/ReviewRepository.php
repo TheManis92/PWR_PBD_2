7534b937561c3555cc195c1566c167bc755fb4be
Avgariat <avgariat.else@gmail.com>
2021-01-25 22:35:41 +0100

    fetch avg rating = [[movie_id, rating], ..]

--- src/Repository/ReviewRepository.php
+++ src/Repository/ReviewRepository.php
@@ -2,49 +2,63 @@
 
 namespace App\Repository;
 
+use App\Entity\Movie;
 use App\Entity\Review;
+use App\QueryResult\MovieAverageRating;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @method Review|null find($id, $lockMode = null, $lockVersion = null)
- * @method Review|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Review|null find($id, $lockMode = NULL, $lockVersion = NULL)
+ * @method Review|null findOneBy(array $criteria, array $orderBy = NULL)
  * @method Review[]    findAll()
- * @method Review[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Review[]    findBy(array $criteria, array $orderBy = NULL, $limit = NULL, $offset = NULL)
  */
-class ReviewRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Review::class);
-    }
+class ReviewRepository extends ServiceEntityRepository {
+	public function __construct(ManagerRegistry $registry) {
+		parent::__construct($registry, Review::class);
+	}
 
-    // /**
-    //  * @return Review[] Returns an array of Review objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('r.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+	public function getMoviesAverageRating() {
+		$conn = $this->getEntityManager()->getConnection();
+		$sql = '
+			SELECT m.id AS movie_id, AVG(r.rating) AS avg_rating FROM movie m 
+				INNER JOIN review r on m.id = r.movie_id 
+				WHERE r.is_accepted = 1 
+				GROUP BY m.id
+		';
+		$stmt = $conn->prepare($sql);
+		$stmt->execute();
+		return $stmt->fetchAllAssociative();
+	}
 
-    /*
-    public function findOneBySomeField($value): ?Review
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+	// /**
+	//  * @return Review[] Returns an array of Review objects
+	//  */
+	/*
+	public function findByExampleField($value)
+	{
+		return $this->createQueryBuilder('r')
+			->andWhere('r.exampleField = :val')
+			->setParameter('val', $value)
+			->orderBy('r.id', 'ASC')
+			->setMaxResults(10)
+			->getQuery()
+			->getResult()
+		;
+	}
+	*/
+
+	/*
+	public function findOneBySomeField($value): ?Review
+	{
+		return $this->createQueryBuilder('r')
+			->andWhere('r.exampleField = :val')
+			->setParameter('val', $value)
+			->getQuery()
+			->getOneOrNullResult()
+		;
+	}
+	*/
 }
